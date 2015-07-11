140445af2b699c24d4a774b474b69e67098f0f54
goliatone <burgosemi@gmail.com>
2015-07-10 23:03:55 -0400

    TEST: WIP adding tests

--- test/impresario_test.js
+++ test/impresario_test.js
@@ -7,14 +7,96 @@ var fixture = path.resolve.bind(path, __dirname, 'fixtures');
 
 sinon.assert.expose(assert, { prefix: '' });
 
-var impresario = require('..');
+var Impresario = require('..');
 
 
 describe('impresario', function(){
 
     describe('constructor', function(){
         it('should provide a DEFAULTS object', function(){
-            assert.isObject(impresario.DEFAULTS);
+            assert.isObject(Impresario.DEFAULTS);
         });
+
+        it('should return a impresario instance if called without new', function(){
+            var conf = Impresario();
+            assert.instanceOf(conf, Impresario);
+        });
+
+        it('should create instance with "optional" and "required" props', function(){
+            var conf = Impresario();
+            assert.ok(conf.required);
+            assert.ok(conf.optional);
+        });
+    });
+
+
+    describe('load' , function(){
+        it('should load default items', function(done){
+            var expected = {
+                url: 'http://localhost',
+                port: '3030',
+                username: 'goliatone'
+            };
+            var A = {url:'http://localhost'};
+            var B = {port: '3030', username: 'goliatone'}
+
+            Impresario({
+            }).on('loaded', function(conf){
+                assert.deepEqual(conf, expected);
+                done();
+            }).load(A, B);
+        });
+
+        it('should throw if required fields are not found', function(done){
+            var expected = Impresario._format(Impresario.REQUIRED_PROPERTY, 'password');
+            Impresario({
+                required: ['password']
+            }).on('error', function(err){
+                assert.equal(expected, err.message);
+                done();
+            }).load();
+        });
+
+        it('should return all keys if "optional" is not defined', function(done){
+            var expected = {
+                url: 'http://localhost',
+                port: '3030',
+                username: 'goliatone'
+            };
+
+            Impresario({
+
+            }).on('loaded', function(conf){
+                assert.deepEqual(conf, expected);
+                done();
+            }).load(expected);
+        });
+
+        it('should return only optional keys if present', function(done){
+            var defaults = {
+                url: 'http://localhost',
+                port: '3030',
+                username: 'goliatone'
+            };
+
+            var expected = {
+                username: 'goliatone'
+            };
+
+            Impresario({
+                optional: ['username']
+            }).on('loaded', function(conf){
+                assert.deepEqual(conf, expected);
+                done();
+            }).load(defaults);
+        });
+    });
+
+    describe('load' , function(){
+        // Impresario({
+        //     name: 'event-stream',
+        //     required: ['host'],
+        //     optional: ['sessionId', 'username', 'password', 'loggerKey']
+        // }).on('loaded', done).load(program);
     });
 });
